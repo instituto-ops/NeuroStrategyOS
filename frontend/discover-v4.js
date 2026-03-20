0e70bafa96e2ca47d9e89f4498387f09d4e47000
instituto-ops <instituto@hipnolawrence.com>
2026-03-20 02:16:25 -0300

    security: remove test files with hardcoded API key

--- frontend/discover-v4.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-const key = "AIzaSyBtVFHJjN3CsUytHfRXzEqxgq5hj9hbcrQ";
-const url = `https://generativelanguage.googleapis.com/v1beta/models?key=${key}`;
-
-https.get(url, (res) => {
-    let data = '';
-    res.on('data', m => data += m);
-    res.on('end', () => console.log(data));
-});
